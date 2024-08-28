c26476703dd63e1efe461d9737c8adf157a66b33
unknown <vani.08.16.22@gmail.comgit config --global user.name yoko-s89>
2024-08-28 23:12:52 +0900

    ホーム画面、サインアップ、ログイン画面作成

---
+++ familytreasures/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+class SignupView(View):
+    def get(self, request):
+        # form = SignupForm()
+        return render(request, "signup.html", context={
+            # "form":form
+        })
+        
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+    
+
+class HomeView(View):
+    # login_url = "login"
+    def get(self, request):
+        return render(request, "home.html")
--- familytreasures/familytreasures/urls.py
+++ familytreasures/familytreasures/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import SignupView,LoginView,HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', SignupView.as_view(), name="signup"),
+    path('', LoginView.as_view(), name="login"),
+    path('home/', HomeView.as_view(), name="home"),
 ]
